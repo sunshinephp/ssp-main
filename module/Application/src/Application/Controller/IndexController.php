bdca06da4c5e1abafb9dd2f2c881f9b0a834bd71
Adam Culp <adamculp@uws.net>
2013-09-13 14:51:20 -0400

    Add about page for the conference.

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -40,6 +40,11 @@ class IndexController extends AbstractActionController
     {
         return new ViewModel();
     }
+
+    public function aboutAction()
+    {
+        return new ViewModel();
+    }
     
     public function scheduleAction()
     {
