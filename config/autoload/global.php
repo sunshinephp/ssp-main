d6dc7808901725e8846d8db7f5283420f604a348
aculp <adamculp@uws.net>
2015-09-01 14:43:13 -0400

    Added call to create DB adapter

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,9 @@
  */
 
 return array(
-    // ...
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ),
+    ),
 );
